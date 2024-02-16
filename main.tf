6c75c5188bf8ae05a4d6a171e9f7508b9d476bea
Shawn Craver <shawn@craver.me>
2024-02-16 11:27:53 -0500

    role assignment doesn't belong here

--- main.tf
+++ main.tf
@@ -104,12 +104,6 @@ resource "azurerm_role_assignment" "static-site-rg-role" {
   principal_id         = data.azuread_service_principal.static-site-sp.object_id
 }
 
-resource "azurerm_role_assignment" "cdn-role" {
-  scope                = azurerm_cdn_profile.static-site-cdn-profile.id
-  role_definition_name = "Contributor"
-  principal_id         = data.azuread_service_principal.static-site-sp.object_id
-}
-
 resource "azurerm_storage_account" "static-site-sa" {
   name                      = "${var.env}0static0site0sa0${random_string.suffix.result}"
   resource_group_name       = azurerm_resource_group.static-site-rg.name
